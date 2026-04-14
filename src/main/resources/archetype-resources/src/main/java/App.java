a2fae776fd1491695288ff63f473d416024c3a71
Pavlo Levchenko <pavlo_levchenko@epam.com>
2026-04-14 21:53:03 +0300

    GIA-35 Configure spring application

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.application.crm.gym;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
