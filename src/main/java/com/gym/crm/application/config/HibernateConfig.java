52e031467cc1fe6840d16449a8fa3d1c7103e078
Pavlo Levchenko <pavlo_levchenko@epam.com>
2026-04-25 21:10:23 +0300

    GIA-69 Configure Hibernate

--- src/main/java/com/gym/crm/application/config/HibernateConfig.java
+++ src/main/java/com/gym/crm/application/config/HibernateConfig.java
@@ -4,7 +4,6 @@ import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 
 import java.util.Properties;
 
@@ -24,10 +23,8 @@ public class HibernateConfig {
     private String password;
 
     @Bean
-    @Lazy
     public SessionFactory sessionFactory() {
         Properties props = new Properties();
-
         props.put("hibernate.connection.driver_class", driver);
         props.put("hibernate.connection.url", url);
         props.put("hibernate.connection.username", username);
@@ -35,12 +32,10 @@ public class HibernateConfig {
         props.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
         props.put("hibernate.show_sql", "true");
         props.put("hibernate.format_sql", "true");
-        props.put("hibernate.hbm2ddl.auto", "none");
-        props.put("hibernate.temp.use_jdbc_metadata_defaults", "false");
+        props.put("hibernate.hbm2ddl.auto", "validate");
 
         return new org.hibernate.cfg.Configuration()
                 .setProperties(props)
-                .addPackage("com.gym.crm.application.model")
                 .buildSessionFactory();
     }
 }
--- src/test/java/com/gym/crm/application/database/HibernateConfigTest.java
+++ src/test/java/com/gym/crm/application/database/HibernateConfigTest.java
@@ -1,13 +1,15 @@
 package com.gym.crm.application.database;
 
 import com.gym.crm.application.config.GymAppConfig;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(SpringExtension.class)
@@ -15,10 +17,29 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class HibernateConfigTest {
 
     @Autowired
-    private ApplicationContext context;
+    private SessionFactory sessionFactory;
 
     @Test
-    void context_ShouldCreateSessionFactoryBean() {
-        assertTrue(context.containsBean("sessionFactory"));
+    void sessionFactory_whenContextLoaded_isNotNull() {
+        assertNotNull(sessionFactory);
+    }
+
+    @Test
+    void sessionFactory_whenCreated_isNotClosed() {
+        assertFalse(sessionFactory.isClosed());
+    }
+
+    @Test
+    void session_whenOpened_isOpen() {
+        try (Session session = sessionFactory.openSession()) {
+            assertTrue(session.isOpen());
+        }
+    }
+
+    @Test
+    void session_whenOpened_isConnectedToDatabase() {
+        try (Session session = sessionFactory.openSession()) {
+            assertTrue(session.isConnected());
+        }
     }
 }
