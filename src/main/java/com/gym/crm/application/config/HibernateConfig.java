b48873f1af48e308d1fc42cfed2645830affd269
Pavlo Levchenko <pavlo_levchenko@epam.com>
2026-04-25 11:55:29 +0300

    GIA-69 Configure Hibernate

--- src/main/java/com/gym/crm/application/config/HibernateConfig.java
+++ src/main/java/com/gym/crm/application/config/HibernateConfig.java
@@ -4,16 +4,11 @@ import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.hibernate5.HibernateTransactionManager;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.context.annotation.Lazy;
 
-import javax.sql.DataSource;
 import java.util.Properties;
 
 @Configuration
-@EnableTransactionManagement
 public class HibernateConfig {
 
     @Value("${db.driver}")
@@ -29,39 +24,23 @@ public class HibernateConfig {
     private String password;
 
     @Bean
-    public DataSource dataSource() {
-        DriverManagerDataSource ds = new DriverManagerDataSource();
-
-        ds.setDriverClassName(driver);
-        ds.setUrl(url);
-        ds.setUsername(username);
-        ds.setPassword(password);
-
-        return ds;
-    }
-
-    @Bean
-    public LocalSessionFactoryBean sessionFactory(DataSource dataSource) {
-        LocalSessionFactoryBean factory = new LocalSessionFactoryBean();
+    @Lazy
+    public SessionFactory sessionFactory() {
         Properties props = new Properties();
 
-        factory.setDataSource(dataSource);
-        factory.setPackagesToScan("com.gym.crm.application.model");
-
+        props.put("hibernate.connection.driver_class", driver);
+        props.put("hibernate.connection.url", url);
+        props.put("hibernate.connection.username", username);
+        props.put("hibernate.connection.password", password);
         props.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
         props.put("hibernate.show_sql", "true");
         props.put("hibernate.format_sql", "true");
         props.put("hibernate.hbm2ddl.auto", "none");
         props.put("hibernate.temp.use_jdbc_metadata_defaults", "false");
 
-        factory.setHibernateProperties(props);
-
-        return factory;
+        return new org.hibernate.cfg.Configuration()
+                .setProperties(props)
+                .addPackage("com.gym.crm.application.model")
+                .buildSessionFactory();
     }
-
-    @Bean
-    public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
-        return new HibernateTransactionManager(sessionFactory);
-    }
-
 }
--- src/test/java/com/gym/crm/application/database/HibernateConfigTest.java
+++ src/test/java/com/gym/crm/application/database/HibernateConfigTest.java
@@ -1,35 +1,24 @@
 package com.gym.crm.application.database;
 
 import com.gym.crm.application.config.GymAppConfig;
-import org.hibernate.SessionFactory;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import javax.sql.DataSource;
-
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = GymAppConfig.class)
 public class HibernateConfigTest {
 
     @Autowired
-    private DataSource dataSource;
-
-    @Autowired
-    private SessionFactory sessionFactory;
-
-    @Autowired
-    private HibernateTransactionManager transactionManager;
+    private ApplicationContext context;
 
     @Test
-    void context_ShouldLoadHibernateBeans() {
-        assertNotNull(dataSource);
-        assertNotNull(sessionFactory);
-        assertNotNull(transactionManager);
+    void context_ShouldCreateSessionFactoryBean() {
+        assertTrue(context.containsBean("sessionFactory"));
     }
 }
