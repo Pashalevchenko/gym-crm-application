a0754aae7d5c7a60ed80c6212f22aa833c2c430c
Pavlo Levchenko <pavlo_levchenko@epam.com>
2026-05-06 20:33:49 +0300

    GIA-94 Increasing test coverage

---
+++ src/test/java/com/gym/crm/application/validation/TrainerValidatorTest.java
@@ -0,0 +1,69 @@
+package com.gym.crm.application.validation;
+
+import com.gym.crm.application.entity.Trainer;
+import com.gym.crm.application.entity.TrainingType;
+import com.gym.crm.application.entity.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class TrainerValidatorTest {
+
+
+    private TrainerValidator trainerValidator;
+
+    @BeforeEach
+    void setUp() {
+        trainerValidator = new TrainerValidator();
+    }
+
+    @Test
+    @DisplayName("Should pass validation when trainer create request is valid")
+    void validateForCreate_whenTrainerIsValid_shouldNotThrowException() {
+        Trainer trainer = Trainer.builder()
+                .user(User.builder()
+                        .firstName("Ivan")
+                        .lastName("Ivanov")
+                        .build())
+                .specialization(TrainingType.builder()
+                        .trainingTypeName("Yoga")
+                        .build())
+                .build();
+
+        assertDoesNotThrow(() -> trainerValidator.validateForCreate(trainer));
+    }
+
+    @Test
+    @DisplayName("Should throw exception when trainer create request is null")
+    void validateForCreate_whenTrainerIsNull_shouldThrowException() {
+        assertThrows(IllegalArgumentException.class, () -> trainerValidator.validateForCreate(null));
+    }
+
+    @Test
+    @DisplayName("Should throw exception when trainer first name is blank")
+    void validateForCreate_whenFirstNameIsBlank_shouldThrowException() {
+        Trainer trainer = Trainer.builder()
+                .user(User.builder()
+                        .firstName(" ")
+                        .lastName("Ivanov")
+                        .build())
+                .build();
+
+        assertThrows(IllegalArgumentException.class, () -> trainerValidator.validateForCreate(trainer));
+    }
+
+    @Test
+    @DisplayName("Should throw exception when trainer last name is blank")
+    void validateForCreate_whenLastNameIsBlank_shouldThrowException() {
+        Trainer trainer = Trainer.builder()
+                .user(User.builder()
+                        .firstName("Ivan")
+                        .lastName(" ")
+                        .build())
+                .build();
+
+        assertThrows(IllegalArgumentException.class, () -> trainerValidator.validateForCreate(trainer));
+    }
+}
