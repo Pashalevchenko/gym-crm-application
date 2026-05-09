d7a23e21a6d864bd363ed5c2aed808e763a54a0e
Pavlo Levchenko <pavlo_levchenko@epam.com>
2026-05-09 11:20:29 +0300

    GIA-98 Setup CI with Sonarcube

--- src/test/java/com/gym/crm/application/config/TestAppConfig.java
+++ src/test/java/com/gym/crm/application/config/TestAppConfig.java
@@ -1,9 +1,38 @@
 package com.gym.crm.application.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
+@ComponentScan(
+        basePackages = "com.gym.crm.application",
+        excludeFilters = {
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = GymAppConfig.class),
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = LiquibaseConfig.class)
+        }
+)
 @PropertySource("classpath:application-test.properties")
-public class TestAppConfig extends GymAppConfig {
+@EnableAspectJAutoProxy
+public class TestAppConfig {
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean(name = "liquibase")
+    public Object liquibase() {
+        return new Object();
+    }
 }
