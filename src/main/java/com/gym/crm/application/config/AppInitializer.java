bd4de9b5f7a897071afa3bc2dd81a63880dc3c7b
Pavlo Levchenko <pavlo_levchenko@epam.com>
2026-05-22 13:48:54 +0300

    GIA-127 Implement levels of logging

--- src/main/java/com/gym/crm/application/config/AppInitializer.java
+++ src/main/java/com/gym/crm/application/config/AppInitializer.java
@@ -1,12 +1,15 @@
 package com.gym.crm.application.config;
 
+import com.gym.crm.application.logging.RestLoggingFilter;
+import com.gym.crm.application.logging.TransactionIdFilter;
+import jakarta.servlet.Filter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return new Class<?>[] {
+        return new Class<?>[]{
                 GymAppConfig.class,
                 HibernateConfig.class
         };
@@ -14,13 +17,22 @@ public class AppInitializer extends AbstractAnnotationConfigDispatcherServletIni
 
     @Override
     protected Class<?>[] getServletConfigClasses() {
-        return new Class<?>[] {
+        return new Class<?>[]{
                 WebConfig.class
         };
     }
 
     @Override
     protected String[] getServletMappings() {
-        return new String[] {"/"};
+        return new String[]{"/"};
+    }
+
+    @Override
+    protected Filter[] getServletFilters() {
+        return new Filter[]
+                {
+                    new TransactionIdFilter(),
+                    new RestLoggingFilter()
+                };
     }
 }
