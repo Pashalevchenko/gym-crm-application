9abbb858d34a08edc0c5acb992be891436c65bf0
Pavlo Levchenko <pavlo_levchenko@epam.com>
2026-05-27 21:28:50 +0300

    GIA-159 Implement health indicators for Actuator

--- src/test/java/com/gym/crm/application/config/TestAppConfig.java
+++ src/test/java/com/gym/crm/application/config/TestAppConfig.java
@@ -3,7 +3,6 @@ package com.gym.crm.application.config;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.PropertySource;
