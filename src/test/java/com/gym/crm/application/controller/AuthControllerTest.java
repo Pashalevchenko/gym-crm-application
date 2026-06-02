31ecf67fe60a2d0964e25018d7ad0b364576db21
Pavlo Levchenko <pavlo_levchenko@epam.com>
2026-06-02 22:44:54 +0300

    GIA-187 Implement logout functionality - add logout business logic

--- src/test/java/com/gym/crm/application/controller/AuthControllerTest.java
+++ src/test/java/com/gym/crm/application/controller/AuthControllerTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
@@ -58,6 +59,17 @@ class AuthControllerTest {
                         .content(mapper.writeValueAsString(request)))
                 .andExpect(status().isOk());
         verify(facade).login(USERNAME, PASSWORD);
+    }
+
+    @Test
+    void logout_shouldReturnOk() throws Exception {
+        String authorizationHeader = "Bearer jwt-token";
+
+        mockMvc.perform(post(BASE_URL + "/logout")
+                        .header(HttpHeaders.AUTHORIZATION, authorizationHeader))
+                .andExpect(status().isOk());
+
+        verify(facade).logout(authorizationHeader);
     }
 
     @Test
